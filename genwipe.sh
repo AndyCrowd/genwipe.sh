5f2d0dcf601b62f5b63ae638fba91c62cb3bebac
AndyCrowd <AndyCrowd@users.noreply.github.com>
2016-07-14 11:06:21 +0200

    Update genwipe.sh

--- genwipe.sh
+++ genwipe.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 ShowExample()
 	{
-	Example+=("#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/urandom oflag=\"noerror,nocache,direct\"");
-	Example+=("#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/random  oflag=\"noerror,nocache,direct\"");
-	Example+=("#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/zero    oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_part_mounts#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/urandom oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_part_mounts#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/random  oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_part_mounts#dd seek=0 of=/dev/$BaseName bs=$Sector_Size count=$Dest_Size if=/dev/zero    oflag=\"noerror,nocache,direct\"");
 
-	Example+=("#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/urandom oflag=\"noerror,nocache,direct\"");
-	Example+=("#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/random  oflag=\"noerror,nocache,direct\"");
-	Example+=("#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/zero    oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_mounts#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/urandom oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_mounts#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/random  oflag=\"noerror,nocache,direct\"");
+	Example+=("$Show_mounts#dd seek=$Start_point of=/dev/$StorageName bs=$Sector_Size count=$Dest_Size if=/dev/zero    oflag=\"noerror,nocache,direct\"");
 
-	Example+=("#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/urandom > /dev/$BaseName");
-	Example+=("#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/random  > /dev/$BaseName");
-	Example+=("#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/zero    > /dev/$BaseName");
+	Example+=("${Show_mounts}${Show_part_mounts}#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/urandom > /dev/$BaseName");
+	Example+=("${Show_mounts}${Show_part_mounts}#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/random  > /dev/$BaseName");
+	Example+=("${Show_mounts}${Show_part_mounts}#pv --stop-at-size -s $((Dest_Size * Sector_Size)) --progress --eta --timer --rate --average-rate < /dev/zero    > /dev/$BaseName");
 
 for GetEx in "${Example[@]}";do
-echo $GetEx;
+echo -e $GetEx;
 done;
 	}
 ShowHelp()
 	{
 echo 'Use valid path to the storage device!
-The script reading data from /sys/block/
+The script is reading data from sysfs located at "/sys/block/".
+E=empty, no mount points found
+M=mount, has mounted partitions
  Usage:
 To show calculated examples for "dd" and "pv"
  genwipe.sh /dev/sdXY
@@ -32,9 +34,15 @@ To update information about partitions use:
  partprobe';
 	}
 if [[ ! -z "$@" ]];then
-	if [[ -e "$@" && -b "$@"  ]];then
+	if [[ -e "$1" && -b "$1"  ]];then
+Get_Dev_mounts=$(lsblk -o "MOUNTPOINT" ${1/[0-9]*/}  | awk '//{if(NR>1)DM=DM sprintf($1)}END{print DM}');
+if [[ -z "$Get_Dev_mounts" ]];then 
+Show_mounts='E';
+else
+Show_mounts='\e[31mM\e[0m';
+fi
 		if [[ "$@" =~ [a-Z][0-9]+$ ]];then 
-			DevPath="$@";
+			DevPath="$1";
 			rmPath="${DevPath/[^*]*\///}";
 			PartBaseName="${rmPath/\//}";
 			DevBaseName="${PartBaseName/[0-9]/}";
@@ -42,16 +50,25 @@ if [[ ! -z "$@" ]];then
 			Part_Start="$(cat /sys/block/$DevBaseName/$PartBaseName/start)";
 			Part_Size="$( cat  /sys/block/$DevBaseName/$PartBaseName/size)";
 			Logical_Sector_Size="$(cat /sys/block/$DevBaseName/queue/logical_block_size)";
-
 			BaseName="$PartBaseName";
 			StorageName="$DevBaseName";
 			Dest_Size="$Part_Size"
 			Sector_Size="$Logical_Sector_Size";
 			Start_point="$Part_Start" 
-			ShowExample $@;	
+Get_Part_mounts=$(findmnt "$1" -o TARGET  | awk '//{if(NR>1)DM=DM sprintf($1)}END{print DM}');
+if [[  -z "$Get_Part_mounts" ]];then
+Show_part_mounts='E';
+else
+Show_part_mounts='\e[31mM\e[0m';
+fi
+			ShowExample "$1";	
 		fi
-		if [[ "$@" =~ [a-Z]+$ ]];then 
-			DevPath="$@";
+
+if [[  -z "$Get_Part_mounts" ]];then
+Show_part_mounts="$Show_mounts"
+fi
+		if [[ "$1" =~ [a-Z]+$ ]];then 
+			DevPath="$1";
 			rmPath="${DevPath/[^*]*\///}";
 			PartBaseName="${rmPath/\//}";
 			DevBaseName="${PartBaseName/[0-9]/}";
@@ -64,7 +81,8 @@ if [[ ! -z "$@" ]];then
 			Dest_Size="$Part_Size";
 			Sector_Size="$Physical_Sector_Size";
 			Start_point=0;
-			ShowExample "$@";
+
+			ShowExample "$1";
 		fi
 	fi
 	if [[ "$@" == '--help' || "$@" == '-h' ]];then
